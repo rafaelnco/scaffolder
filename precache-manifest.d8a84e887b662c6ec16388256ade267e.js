e0d78d9872b063e9eeaefc06e9151a8b8e4ed0cf
rafael <6116829+rafaelnco@users.noreply.github.com>
2019-09-09 09:23:22 -0300

    base

---
+++ precache-manifest.d8a84e887b662c6ec16388256ade267e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "092c0d75fa7f6744681fda12032cc789",
+    "url": "./index.html"
+  },
+  {
+    "revision": "21ff46230a9befed9e7c",
+    "url": "./static/css/main.2f8efb9f.chunk.css"
+  },
+  {
+    "revision": "72540badb8568f7e1c44",
+    "url": "./static/js/2.95557cb7.chunk.js"
+  },
+  {
+    "revision": "21ff46230a9befed9e7c",
+    "url": "./static/js/main.4688a072.chunk.js"
+  },
+  {
+    "revision": "6bc1ec55888e0aae57e1",
+    "url": "./static/js/runtime~main.f4f79316.js"
+  }
+]);
